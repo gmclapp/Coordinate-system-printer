ba252bdd4924c9c33cc3efbaad6f032e3ab2a3e7
Glenn Clapp <glenn.m.clapp@gmail.com>
2018-06-21 19:01:45 -0400

    Added auto test input
    Close issue #13

--- A_star tester.py
+++ A_star tester.py
@@ -1,12 +1,39 @@
 import sanitize_inputs as si
 import A_star
 
-print("Enter the coordinates of the obstacle.")
-o_pos = si.get_coords()
-o_r = si.get_real_number("Enter the radius of the obstacle >>> ", lower = 0)
-o_h = si.get_real_number("Enter the height of the obstacle. (leave blank for infinite height). >>> ", lower = 0)
+def manual_entry():
+    print("Enter the coordinates of the obstacle.")
+    o_pos = si.get_coords()
+    o_r = si.get_real_number("Enter the radius of the obstacle >>> ", lower = 0)
+    o_h = si.get_real_number("Enter the height of the obstacle. (leave blank for infinite height). >>> ", lower = 0)
 
-obst = A_star.obstacle(o_pos, o_r, o_h)
+    obst = A_star.obstacle(o_pos, o_r, o_h)
+
+    print("Enter the starting position. >>> ")
+    start = A_star.node(si.get_coords())
+
+    print("Enter the payload position. >>> ")
+    end = A_star.node(si.get_coords())
+
+    n = si.get_integer("Enter the approximate number of nodes along the path. >>> ", lower = 0)
+    
+def auto_entry():
+    o_pos = si.col_vec([2,2,0])
+    o_r = 2.5
+    o_h = float('Inf')
+
+    obst = A_star.obstacle(o_pos, o_r, o_h)
+
+    start = A_star.node(si.col_vec([0,0,0]))
+
+    end = A_star.node(si.col_vec([2,3,3]))
+
+    n = 25
+
+    return(obst, start, end, n)
+
+obst, start, end, n = auto_entry()
+
 print("Cool obstacle!\nIt is located at:\nX: ",
       obst.loc.x,"\n",
       "Y: ", obst.loc.y,"\n",
@@ -14,24 +41,16 @@ print("Cool obstacle!\nIt is located at:\nX: ",
       "It has a radius:\nR: ", obst.r,"\n",
       "It has a height:\nh: ", obst.h,"\n", sep='')
 
-print("Enter the starting position. >>> ")
-start = A_star.node(si.get_coords())
-
 print("Thanks, the robot gripper will start at:\nX: ",
       start.loc.x,"\n",
       "Y: ", start.loc.y,"\n",
       "Z: ", start.loc.z,"\n",sep='')
 
-print("Enter the payload position. >>> ")
-end = A_star.node(si.get_coords())
-
 print("Ok, the payload will be retreived from:\nX: ",
       end.loc.x,"\n",
       "Y: ", end.loc.y,"\n",
       "Z: ", end.loc.z,"\n",sep='')
-
-n = si.get_integer("Enter the approximate number of nodes along the path. >>> ", lower = 0)
-
+    
 obst.collision_detect(start)
 
 path = A_star.generate_path(start, end, obst)
