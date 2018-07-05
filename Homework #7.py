3510a810879059850f57941f19d5fe77aa5e91f9
Glenn Clapp <glenn.m.clapp@gmail.com>
2018-07-05 14:05:53 -0400

    Ready for submission

--- Homework #7.py
+++ Homework #7.py
@@ -1,13 +1,13 @@
 import robotics
 import numpy as np
 import sympy
-##import robotics_sympy as rs
-##
-##th1, d, th3 = sympy.symbols(['theta1','d','theta3'])
-##
-##T01 = rs.DH(0, 0, th1, 5)
-##T12 = rs.DH(-90, 0, 0, d)
-##T23 = rs.DH(-90, 0, th3, 0)
+import robotics_sympy as rs
+
+th1, d, th3 = sympy.symbols(['theta1','d','theta3'])
+
+T01 = rs.DH(0, 0, th1, 5)
+T12 = rs.DH(-90, 0, 0, d)
+T23 = rs.DH(-90, 0, th3, 0)
 
 sol1_T01 = robotics.prettymat(robotics.DH(0, 0, 64.1, 5))
 sol1_T12 = robotics.prettymat(robotics.DH(-90, 0, -90, 3.7))
@@ -23,8 +23,6 @@ sol2_T = sol2_T01*sol2_T12*sol2_T23
 
 P3tip = np.array([[0],[-2],[0],[1]])
 
-#P_A = robotics.dot_list([T_AB, T_BC, T_CD, P_D])
-
 P0sol1 = robotics.prettymat(robotics.dot_list([sol1_T01,
                                               sol1_T12,
                                               sol1_T23,
@@ -35,11 +33,4 @@ P0sol2 = robotics.prettymat(robotics.dot_list([sol2_T01,
                                               sol2_T23,
                                               P3tip]))
 
-##robotics.print_mat(sol1_T01)
-##robotics.print_mat(sol1_T12)
-##robotics.print_mat(sol1_T23)
-##
-##robotics.print_mat(sol2_T01)
-##robotics.print_mat(sol2_T12)
-##robotics.print_mat(sol2_T23)
 
