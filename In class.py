9b91f15fe43ac4ef1935ba239268ba32be310b2a
Glenn Clapp <glenn.m.clapp@gmail.com>
2018-07-19 19:11:10 -0400

    Fixed cross product errors and attempted to expand to the two link version
    This code is not currently yielding the correct results.

--- In class.py
+++ In class.py
@@ -15,16 +15,33 @@ def get_R(axis, angle):
     return(robotics.prettymat(R))
 
 L1 = 20
+L2 = 15
 w1z = 3
-theta_list = [0,45,60,90]
+w2z = 4
+theta1_list = [0,0,20]
+theta2_list = [0,90,50]
 
 omega1 = np.array([0,0,w1z])
+omega2 = np.array([0,0,w2z])
+
 pos1 = np.array([L1,0,0])
+pos2 = np.array([L2,0,0])
 
-vel1 = np.cross(omega1, L1)
+vel1 = np.cross(omega1, pos1) # velocity of link1 tip in frame 1
 
-for th in theta_list:
-    R = get_R('z',th)
-    vel0 = np.dot(R, vel1)
-    print(vel0)
+for i in range(len(theta1_list)):
+    R01 = get_R('z', theta1_list[i])
+    R12 = get_R('z', theta2_list[i])
+    R02 = np.dot(R01,R12)
+
+    omega2in2 = omega2 + np.dot(R12, omega1)
+    vel2 = np.cross(omega2in2, pos2) # velocity of link 2 tip in frame 2
+    
+    v2in1 = vel1 + np.dot(R12, vel2)
+    print(v2in1)
+    
+    
+
+    
+
     
